2bfcd0faa72778cd4a73cda3efc653cbe8788200
Feito, Nazareno <nfeito@netsuite.com>
2018-08-01 11:06:59 -0400

    Generalize scraping function that returns the generator Make the code functional in order to support the generalization.

--- scraper.py
+++ scraper.py
@@ -1,19 +1,18 @@
 """ """
 import logging
 from time import time
-from functools import wraps
+from functools import partial, wraps
 from collections import Counter
 
 import pandas as pd
 import oci
-
 
 # The lifecycle_state parameter only allows one at a time (per fetch), python-sdk limitation.
 LC_STATE={
         'INSTANCE': 'RUNNING',
         'VOLUME': 'AVAILABLE',
         'VCN': 'AVAILABLE',
-        'LOADBALANCER': 'ACTIVE',
+        'LOAD_BALANCER': 'ACTIVE',
         }
 
 
@@ -44,16 +43,15 @@ def get_subscribed_regions():
     return identity_client.list_region_subscriptions(config['tenancy']).data
 
 
-def create_instance_dataframe(config, compartments):
+def create_instance_dataframe(scrape_regional_results):
     """ """
-    df = pd.DataFrame({}, columns=['Region', 'Compartment', 'AD', 'Name', 'Shape', 
+    df = pd.DataFrame({}, columns=['Compartment', 'AD', 'Name', 'Shape', 
                                    'Defined tags', 'Freeform tags', 'Time created'])
 
-    for results in scrape_compute(config, compartments):
-        (compartment, instances) = results
+    for compartment_results in scrape_regional_results:
+        (compartment, instances) = compartment_results
         if instances:
-            df_tmp = pd.DataFrame({'Region': config['region'], 
-                                   'Compartment': compartment, 
+            df_tmp = pd.DataFrame({'Compartment': compartment, 
                                    'AD': [x.availability_domain for x in instances],
                                    'Name': [x.display_name for x in instances],
                                    'Shape': [x.shape for x in instances],
@@ -66,17 +64,16 @@ def create_instance_dataframe(config, compartments):
     return df
 
 
-def create_volume_dataframe(config, compartments):
+def create_volume_dataframe(scrape_regional_results):
     """ """
-    df = pd.DataFrame({}, columns=['Region', 'Compartment', 'AD', 'Name', 
+    df = pd.DataFrame({}, columns=['Compartment', 'AD', 'Name', 
                                     'Size GB', 'Size MB', 'Defined tags', 
                                     'Freeform tags', 'Time created'])
 
-    for results in scrape_storage(config, compartments):
-        (compartment, volumes) = results
+    for compartment_results in scrape_regional_results:
+        (compartment, volumes) = compartment_results
         if volumes:
-            df_tmp = pd.DataFrame({'Region': config['region'],
-                                   'Compartment': compartment, 
+            df_tmp = pd.DataFrame({'Compartment': compartment, 
                                    'AD': [x.availability_domain for x in volumes],
                                    'Name': [x.display_name for x in volumes],
                                    'Size MB': [x.size_in_mbs for x in volumes],
@@ -90,87 +87,65 @@ def create_volume_dataframe(config, compartments):
     return df
 
 
-def create_vcn_dataframe(config, compartments):
+def create_vcn_dataframe(scrape_regional_results):
     """ """
-    df = pd.DataFrame({}, columns=['Region', 'Compartment', 'CIDR block', 'Name', 
+    df = pd.DataFrame({}, columns=['Compartment', 'CIDR block', 'Name', 
                                     'DNS label', 'VCN domain name', 'Defined tags', 
                                     'Freeform tags', 'Time created'])
                                             
-    for results in scrape_network(config, compartments):
-        (compartment, networks) = results
+    for compartment_results in scrape_regional_results:
+        (compartment, networks) = compartment_results
         if networks:
-            df_tmp = pd.DataFrame({'Region': config['region'], 
-                                    'Compartment': compartment, 
-                                    'CIDR block': [x.cidr_block for x in networks],
-                                    'Name': [x.display_name for x in networks],
-                                    'DNS label': [x.dns_label for x in networks],
-                                    'VCN domain name': [x.vcn_domain_name for x in networks],
-                                    'Defined tags': [x.defined_tags for x in networks],
-                                    'Freeform tags': [x.freeform_tags for x in networks],
-                                    'Time created': [x.time_created for x in networks],
-                                    })
+            df_tmp = pd.DataFrame({'Compartment': compartment, 
+                                   'CIDR block': [x.cidr_block for x in networks],
+                                   'Name': [x.display_name for x in networks],
+                                   'DNS label': [x.dns_label for x in networks],
+                                   'VCN domain name': [x.vcn_domain_name for x in networks],
+                                   'Defined tags': [x.defined_tags for x in networks],
+                                   'Freeform tags': [x.freeform_tags for x in networks],
+                                   'Time created': [x.time_created for x in networks],
+                                   })
             df = pd.concat([df, df_tmp], ignore_index=True, sort=False)
 
     return df
 
 
-def create_loadbalancer_dataframe(config, compartments):
+def create_loadbalancer_dataframe(scrape_f):
     """ """
-    df = pd.DataFrame({}, columns=['Region', 'Compartment', 'Name', 'Shape name', 'Time created'])
+    df = pd.DataFrame({}, columns=['Compartment', 'Name', 'Shape name', 'Time created'])
                                             
-    for results in scrape_loadbalancers(config, compartments):
+    for results in scrape_f:
         (compartment, loadbalancers) = results
         if loadbalancers:
-            df_tmp = pd.DataFrame({'Region': config['region'], 
-                                    'Compartment': compartment, 
-                                    'Name': [x.display_name for x in loadbalancers],
-                                    'Shape name': [x.shape_name for x in loadbalancers],
-                                    'Time created': [x.time_created for x in loadbalancers],
-                                    })
+            df_tmp = pd.DataFrame({'Compartment': compartment, 
+                                   'Name': [x.display_name for x in loadbalancers],
+                                   'Shape name': [x.shape_name for x in loadbalancers],
+                                   'Time created': [x.time_created for x in loadbalancers],
+                                   })
             df = pd.concat([df, df_tmp], ignore_index=True, sort=False)
 
     return df
 
 
 @timer
-def scrape_compute(config, compartments):
-    """ """
-    compute = oci.core.ComputeClient(config)
+def scrape_service_elements(compartments, list_function, lc_state):
+    """ This function scrapes different service elements, from each service, 
+        Compute, Storage, Network, etc. The elements are the constituents of
+        the service such as instances, volumes, vcns, load balancers, etc.
+
+        It receives a list of compartments, a list function which already
+        brings the oci object client (e.g. ComputeClient) and the lifecycle state.
+
+        It will yield for each compartment the compartment name and list of elements.
+    """
     for c_name, c_id in compartments.items():
-        instances_raw = compute.list_instances(c_id, lifecycle_state=LC_STATE['INSTANCE'])
-        instances = instances_raw.data
-        while instances_raw.has_next_page:
-            instances_raw = compute.list_instances(c_id, lifecycle_state=LC_STATE['INSTANCE'],
-                                                  page=instances_raw.next_page)
-            instances.extend(instances_raw.data)
-        yield (c_name, instances)
-
-
-@timer
-def scrape_storage(config, compartments):
-    """ """
-    volume = oci.core.BlockstorageClient(config)
-    for c_name, c_id in compartments.items():
-        block_volumes = volume.list_volumes(c_id, lifecycle_state=LC_STATE['VOLUME']).data
-        yield (c_name, block_volumes)
-
-
-@timer
-def scrape_network(config, compartments):
-    """ """
-    network = oci.core.VirtualNetworkClient(config)
-    for c_name, c_id in compartments.items():
-        vcns = network.list_vcns(c_id, lifecycle_state=LC_STATE['VCN']).data
-        yield (c_name, vcns)
-
-
-@timer
-def scrape_loadbalancers(config, compartments):
-    """ """
-    lb = oci.load_balancer.LoadBalancerClient(config)
-    for c_name, c_id in compartments.items():
-        lbs = lb.list_load_balancers(c_id, lifecycle_state=LC_STATE['LOADBALANCER']).data
-        yield (c_name, lbs)
+        elements_raw = list_function(c_id, lifecycle_state=lc_state)
+        elements = elements_raw.data
+        while elements_raw.has_next_page:
+            elements_raw = list_function(c_id, lifecycle_state=lc_state,
+                                                  page=elements_raw.next_page)
+            elements.extend(elements_raw.data)
+        yield (c_name, elements)
 
 
 @timer
@@ -186,12 +161,22 @@ def scrape_a_region(region):
     if not compartments:
         raise ValueError('could not find any compartments.')
 
+    # We generalize in order to have one function to process all elements 
+    # and not one per each. Shorter and easier to make changes in the future.
+    scrape_elements = partial(scrape_service_elements, compartments)
+
+    instances = scrape_elements(oci.core.ComputeClient(config).list_instances, LC_STATE['INSTANCE'])
+    volumes = scrape_elements(oci.core.BlockstorageClient(config).list_volumes, LC_STATE['VOLUME'])
+    vcns = scrape_elements(oci.core.VirtualNetworkClient(config).list_vcns, LC_STATE['VCN'])
+    loadbalancers = scrape_elements(oci.load_balancer.LoadBalancerClient(config).list_load_balancers, 
+                                    LC_STATE['LOAD_BALANCER'])
+    
     # We generate a dataframe per SubService, e.g. Compute->instance, Storage->volume
     # Network->vcn, Loadbalancer->loadbalancer.
-#    create_instance_dataframe(config, compartments).to_csv(f'instance_{region.region_name}.csv')
-#    create_volume_dataframe(config, compartments).to_csv(f'volume_{region.region_name}.csv')
-#    create_vcn_dataframe(config, compartments).to_csv(f'vcn_{region.region_name}.csv')
-    create_loadbalancer_dataframe(config, compartments).to_csv(f'loadbalancer_{region.region_name}.csv')
+    create_instance_dataframe(instances).to_csv(f'output/instance_{region.region_name}.csv')
+    create_volume_dataframe(volumes).to_csv(f'output/volume_{region.region_name}.csv')
+    create_vcn_dataframe(vcns).to_csv(f'output/vcn_{region.region_name}.csv')
+    create_loadbalancer_dataframe(loadbalancers).to_csv(f'output/loadbalancer_{region.region_name}.csv')
 
 
 @timer
